7c14044c7a43228cbc108b2ffca3cbde325b6519
one_true_ <piranin@soccer.tnc.ne.jp>
2019-08-06 14:58:04 +0900

    最低気温も表示

--- proconBot.rb
+++ proconBot.rb
@@ -107,9 +107,10 @@ class ProconBot
 
     message = ""
     forecasts.each { |f|
-      max_temperature = f.dig("temperature","max","celsius")
+      max_temp = f.dig("temperature","max","celsius")
+      min_temp = f.dig("temperature","min","celsius")
       message += "#{f["dateLabel"]}（#{f["date"]}）の#{city}の天気は「#{f["telop"]}」"
-      message += "、最高気温は#{max_temperature}℃" unless max_temperature.nil?
+      message += "、最高気温は#{max_temp}℃、最低気温は#{min_temp}℃" unless max_temp.nil?
       message += "\n"
     }
     message
